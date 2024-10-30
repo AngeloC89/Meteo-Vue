95f5122b7a58783ff3b9254e7e4e2bdef0e9caa3
AngeloC89 <Angelociulla89@gmail.com>
2024-10-30 19:15:27 +0100

    added serach-bar-componet

---
+++ src/components/SearchBarComponent.vue
@@ -0,0 +1,79 @@
+<template>
+    <section class="d-flex align-items-center justify-content-end">
+        <div id="search">
+            <input type="search" v-model.trim="store.options_city.params.name" @keyup.enter="fetchCity">
+            <button @click="fetchCity">Search</button>
+        </div>
+    </section>
+</template>
+
+<script>
+    import { store } from '../store.js';
+    import axios from 'axios';
+    export default {
+        name: 'SearchBarComponent',
+        data() {
+            return {
+                store
+            }
+        },
+        methods: {
+            async fetchCity() {
+                if (this.store.options_city.params.name.length < 2) return;
+                try {
+                    const response = await axios.get(
+                        this.store.apiBaseSearch,
+                        this.store.options_city
+                    );
+                    this.store.city = response.data.results;
+                    console.log(this.store.city);
+                    this.store.options_climate.params.latitude = this.store.city[0].latitude;
+                    this.store.options_climate.params.longitude = this.store.city[0].longitude;
+                    this.fetchClimate();//This function is called after performing city search and finds the weather data for the city
+                } catch (error) {
+                    console.error("Errore nel recupero dei dati", error);
+                }
+
+            },
+
+            async fetchClimate() {
+
+                try {
+                    const response = await axios.get(
+                        this.store.apiBaseClimate,
+                        this.store.options_climate
+                    );
+                    this.store.climate = response.data;
+                    console.log(this.store.climate);
+                } catch (error) {
+                    console.error("Errore nel recupero dei dati", error);
+                }
+            },
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    // section for search the city
+    #search {
+        width: 500px;
+        height: 200px;
+        display: flex;
+        justify-content: end;
+        align-items: center;
+
+        input {
+            width: 300px;
+            height: 40px;
+            font-size: 20px;
+            margin-right: 15px;
+        }
+
+        button {
+            width: 100px;
+            height: 40px;
+            font-size: 20px;
+        }
+    }
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,7 @@
 <template>
     <div id="home" class="container">
         <!-- section for search the city -->
-        <section class="d-flex align-items-center justify-content-end">
-            <div id="search">
-                <input type="search" v-model.trim="store.options_city.params.name" @keyup.enter="fetchCity">
-                <button @click="fetchCity">Search</button>
-            </div>
-        </section>
+        <SearchBarComponent />
 
         <!-- current component -->
         <CurrentComponent />
@@ -23,9 +18,11 @@
     import axios from 'axios';
     import CurrentComponent from '@/components/CurrentComponent.vue';
     import DailyComponent from '@/components/DailyComponent.vue';
+import SearchBarComponent from '@/components/SearchBarComponent.vue';
     export default {
         name: 'HomePage',
         components: {
+            SearchBarComponent,
             CurrentComponent,
             DailyComponent
         },
@@ -37,37 +34,9 @@
         },
         methods: {
             //this function call the api with the search bar
-            async fetchCity() {
-                if (this.store.options_city.params.name.length < 2) return;
-                try {
-                    const response = await axios.get(
-                        this.store.apiBaseSearch,
-                        this.store.options_city
-                    );
-                    this.store.city = response.data.results;
-                    console.log(this.store.city);
-                    this.store.options_climate.params.latitude = this.store.city[0].latitude;
-                    this.store.options_climate.params.longitude = this.store.city[0].longitude;
-                    this.fetchClimate();//This function is called after performing city search and finds the weather data for the city
-                } catch (error) {
-                    console.error("Errore nel recupero dei dati", error);
-                }
-
-            },
+           
             // this function call the api with the coordinates from city array  and get the climate data
-            async fetchClimate() {
-
-                try {
-                    const response = await axios.get(
-                        this.store.apiBaseClimate,
-                        this.store.options_climate
-                    );
-                    this.store.climate = response.data;
-                    console.log(this.store.climate);
-                } catch (error) {
-                    console.error("Errore nel recupero dei dati", error);
-                }
-            },
+           
         },
     }
 </script>
@@ -77,30 +46,6 @@
         width: 100%;
         height: 100vh;
         background-color: rgb(168, 165, 165);
-
-
-        // section for search the city
-        #search {
-            width: 500px;
-            height: 200px;
-            display: flex;
-            justify-content: end;
-            align-items: center;
-
-            input {
-                width: 300px;
-                height: 40px;
-                font-size: 20px;
-                margin-right: 15px;
-            }
-
-            button {
-                width: 100px;
-                height: 40px;
-                font-size: 20px;
-            }
-        }
-
 
     }
 </style>
